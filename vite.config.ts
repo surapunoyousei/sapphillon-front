7cd7c11725fc5fd94c44fe191c2264aa4d1f7ab7
Ryosuke Asano <s1F102400011@iniad.org>
2025-08-09 16:03:12 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react(), tailwindcss()],
+  plugins: [
+    deno(),
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 8999,
+  },
 });
