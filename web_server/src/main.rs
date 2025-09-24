9d5c75cc8571c5a0dfe47ecdf51e82ec5a884e54
Yuta Takahashi <y.highbridge04@gmail.com>
2025-09-24 09:19:46 +0000

    feat: update Cargo.toml and Cargo.lock for actix-web dependencies; add build script and main server implementation

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use actix_web_static_files::ResourceFiles;
+
+include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listen = std::env::var("LISTEN").unwrap_or_else(|_| "127.0.0.1:8081".into());
+    let server = HttpServer::new(|| {
+        let generated = generate();
+        App::new().service(ResourceFiles::new("/", generated))
+    })
+    .bind(listen)?;
+
+    if let Some(addr) = server.addrs().first() {
+        println!("{:05}", addr.port());
+    }
+
+    let handle = actix_web::rt::spawn(server.run());
+
+    handle.await?
 }
